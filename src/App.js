d8ded523bf27da28bc2af0b9635efaa12ec47cf5
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-03-05 22:28:02 -0500

    React-set-up + Nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+import About from "./components/About/About";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Portfolio from "./components/Portfolio/Portfolio";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        {/* <!-- Navigation Bar --> */}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="index.html">Mike Tkachuk</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item nav-li">
+                <Link className="nav-link " to="/portfolio">Portfolio</Link>
+
+              </li>
+            <li className="nav-item nav-li active">
+              <Link className="nav-link " to="/">About Me</Link>
+            </li>
+            <li className="nav-item nav-li">
+            <Link className="nav-link " to="/">Contact</Link>
+            </li>
+            </ul>
+          </div>
+        </nav>
+
+      <div>
+
+
+        <Route exact path="/" component={About} />
+        <Route path="/contact" component={ContactUs} />
+        <Route path="/portfolio" component={Portfolio} />
+      </div>
+      </Router>
+    </div >
   );
 }
 
