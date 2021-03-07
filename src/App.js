5ef873163483de5f5fb19dd755db9c2dd006e35e
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-03-06 20:34:07 -0500

    portfolio page beta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 // Tools
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+
+//Pages
+import Portfolio from './pages/Portfolio'
+import About from './pages/About'
+import ContactUs from './pages/ContactUs'
 
 // Components
 import Navigation from "./components/Navigation.js";
@@ -11,7 +16,19 @@ import Navigation from "./components/Navigation.js";
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navigation />
+      <div className="container mt-2" style={{ marginTop: 40 }}>
+        <Switch>
+          <Route exact path="/">
+          </Route>
+          <Route path="/portfolio">
+          </Route>
+          <Route path="/contact">
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,41 +1,35 @@
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+
 
 // Pages
 import About from "../pages/About";
 import ContactUs from "../pages/ContactUs";
 import Portfolio from "../pages/Portfolio";
+import App from '../App';
 
 const navigation = () => {
     return (
         <div>
-            {/* <!-- Navigation Bar --> */}
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="index.html">Mike Tkachuk</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <Router>
-                            <li className="nav-item nav-li">
-                                <Link className="nav-link " to="/portfolio">Portfolio</Link>
 
-                            </li>
-                            <li className="nav-item nav-li active">
-                                <Link className="nav-link " to="/">About Me</Link>
-                            </li>
-                            <li className="nav-item nav-li">
-                                <Link className="nav-link " to="/contact">Contact</Link>
-                            </li>
-                            <Route exact path="/" component={About} />
-                            <Route path="/contact" component={ContactUs} />
-                            <Route path="/portfolio" component={Portfolio} />
-                        </Router>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Mike Tkachuk</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="/">About</Nav.Link>
+                        <Nav.Link href="/contact">Contact Me</Nav.Link>
+                   </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+                    <Router>
+                        <Route exact path="/" component={About} />
+                        <Route path="/contact" component={ContactUs} />
+                        <Route path="/portfolio" component={Portfolio} />
+                    </Router>
+            </div>
     )
 }
 
