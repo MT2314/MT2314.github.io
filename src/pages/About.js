7a293212a8efa5d3536cf11e4d46846453d672b8
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-03-06 22:31:18 -0500

    ..

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import Portfolio from './Portfolio'
 import Abouttext from '../components/Abouttext'
 import './aboutme.css'
